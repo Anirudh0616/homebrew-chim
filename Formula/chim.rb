ff5a2a034ec77e686fcb4b71dc0894abf524354b
Anirudh0616 <ani.b.0616@gmail.com>
2026-05-03 17:59:33 +0530

    v0.1.4

--- Formula/chim.rb
+++ Formula/chim.rb
@@ -2,7 +2,7 @@ class Chim < Formula
 
   desc "Controlled Vim - vim but with ctrl key shortcuts"
   homepage "https://github.com/Anirudh0616/chim-text-editor"
-  url "https://github.com/Anirudh0616/chim-text-editor/archive/refs/tags/v0.1.2.tar.gz"
+  url "https://github.com/Anirudh0616/chim-text-editor/archive/refs/tags/v0.1.4.tar.gz"
   sha256 "758a6a87c9ef89b90e2d090b19c9f86799744936e076ad67eb2fcb3d4e860865"
   license "MIT"
 
